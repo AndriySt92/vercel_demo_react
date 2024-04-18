5c55b0e8c9022878ae017973dd952fb77ae3d552
AndriySt92 <andriyua-1992@mail.ru>
2024-04-18 19:02:58 +0300

    init

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Vercel demo test
         </a>
       </header>
     </div>
